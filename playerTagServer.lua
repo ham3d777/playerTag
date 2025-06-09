b5e3c558bcae3d0f6e121179ad6222b0b6b48d1f
ham3d777 <ganekgankowski@gmail.com>
2025-06-09 22:14:35 +0200

    Add files via upload

---
+++ playerTagServer.lua
@@ -0,0 +1,26 @@
+--[[
+
+you can set data from leaderstats, attributes, dataStore, etc. I used rawData
+
+]]
+local rawStats = {
+	cash = 10,
+	gems = 100,
+}
+
+local newPlayersTag = {} -- store players tag
+
+game.Players.PlayerAdded:Connect(function(player)
+	player.CharacterAdded:Connect(function(char)
+		if char then
+			newPlayersTag[player.UserId] = require(game.ReplicatedStorage.playerTag.playerTag).new(player, rawStats)
+			newPlayersTag[player.UserId]:init()
+		end
+	end)
+end)
+
+game.Players.PlayerRemoving:Connect(function(player)
+	if player.Character then
+		newPlayersTag[player.UserId]:destroyTag()
+	end
+end)
